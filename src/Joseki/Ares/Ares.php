21349042bde5a520d5d7af6a37ab7b85fd65f33e
castamir <miras.paulik@email.cz>
2015-11-03 00:26:24 +0100

    Soap client: fixed dual failure possibility from SoapClient costructor due wsdl unavailibility

--- src/Joseki/Ares/Ares.php
+++ src/Joseki/Ares/Ares.php
@@ -13,14 +13,18 @@ class Ares
         $url = sprintf('http://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi?ico=%s', $id);
         $record = BasicParser::parse(file_get_contents($url));
 
-        if (class_exists('SoapClient')) {
-            $client = @new SoapClient("http://adisrws.mfcr.cz/adistc/axis2/services/rozhraniCRPDPH.rozhraniCRPDPHSOAP");
-            if ($client instanceof SoapClient) {
+        if (class_exists('SoapClient') && @file_get_contents('http://adisrws.mfcr.cz/adistc/axis2/services/rozhraniCRPDPH.rozhraniCRPDPHSOAP') !== false) {
+            // todo check for failure and throw custom exception
+            try {
+                $client = new SoapClient("http://adisrws.mfcr.cz/adistc/axis2/services/rozhraniCRPDPH.rozhraniCRPDPHSOAP", ['exceptions' => true]);
+
                 $contact = new TaxReliabilityRequest ($id);
                 $params = ["StatusNespolehlivyPlatceRequest" => $contact];
                 $response = $client->__soapCall("getStatusNespolehlivyPlatce", $params);
 
                 $record = TaxReliabilityParser::parse($response, $record);
+            } catch (\SoapFault $e) {
+
             }
         }
 
